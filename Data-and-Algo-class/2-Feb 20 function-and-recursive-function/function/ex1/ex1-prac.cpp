42424987a8dac5dbaace5881007b5cba99ffa63e
SeawaterEtc <cholsaseawater@gmail.com>
2024-05-24 09:41:01 +0700

    more changes

--- Data-and-Algo-class/2-Feb 20 function-and-recursive-function/function/ex1/ex1-prac.cpp
+++ Data-and-Algo-class/2-Feb 20 function-and-recursive-function/function/ex1/ex1-prac.cpp
@@ -1,1 +1,22 @@
-//...  
+// write cpp for Write a program that asks user for an integer number and find the sum of all natural numbers up to that input number: sum(n) = 1 + 2 + 3 + … + n
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    cout << "Q1: write cpp for Write a program that asks user for an integer number and find the sum of all natural numbers up to that input number: sum(n) = 1 + 2 + 3 + … + n. The number is randomized between 1-100: " << endl;
+
+    srand((time(nullptr))); // need this before using rand() 
+
+    int lb = 1, ub = 100;
+
+    int n = rand() % (ub - lb + 1) + lb;
+
+    int sum = n * (n + 1) / 2;
+
+    cout << ">> Answer1: Sum of natural numbers up to " << n << " is: " << sum << endl;
+
+    return 0;
+}
--- Data-and-Algo-class/2-Feb 20 function-and-recursive-function/function/ex2/ex2-prac.cpp
+++ Data-and-Algo-class/2-Feb 20 function-and-recursive-function/function/ex2/ex2-prac.cpp
@@ -1,1 +1,49 @@
-//...  
+// Calculate Fibonacci F(0) = 0, F(1) = 1
+// F(n) = F(n - 1), F(n - 2)
+
+
+#include <iostream>
+using namespace std;
+
+// Function to calculate the nth Fibonacci number
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    int a = 0, b = 1, c;
+    for (int i = 2; i <= n; ++i) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+
+
+int main() {
+    srand(time(0));
+    
+    int lb = 1, ub = 5;
+
+    int n = rand() % (ub - lb + 1) + lb;
+
+    // cout << "Enter a positive integer: ";
+    // cin >> n;
+
+    int a = 0, b = 1, c;
+    for (int i = 2; i <= n; ++i) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+
+    if (n < 0) {
+        cout << "Invalid input. Please enter a non-negative integer.\n";
+    } else {
+        cout << "F(" << n << ") = " << b << "\n";
+    }
+
+    return 0;
+}
